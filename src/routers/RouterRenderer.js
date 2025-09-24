29f5e3a81b8ff46f2a7b37bc65389e117ed197eb
CardboardPL <mrplwaschilling@gmail.com>
2025-09-24 16:43:46 +0800

    Update RouterRenderer.js
    - Made router a private field
    - Added logic to handle 404 errors in renderPathMain()

--- src/routers/RouterRenderer.js
+++ src/routers/RouterRenderer.js
@@ -2,22 +2,31 @@ import { Router } from './Router.js';
 
 export class RouterRenderer {
     #main;
+    #router;
+    #fallbackHTML;
 
-    constructor(main, router) {
+    constructor(main, router, fallbackHTML) {
         if (!(router instanceof Router)) throw new Error('Aborted RouterRenderer Setup: "router" must be an instance of "Router"');
         if (!(main instanceof HTMLElement)) throw new Error('Aborted RouterRenderer Setup: "main" must be an instance of "HTMLElement"');
 
         this.#main = main;
-        this.router = router;
+        this.#router = router;
+        this.#fallbackHTML = fallbackHTML;
     }
 
     renderPathMain(absPath) {
-        const segmentHTML = this.router.getSegmentHTML(absPath);
+        const segmentHTML = this.#router.getSegmentHTML(absPath);
+
+        if (!segmentHTML) {
+            this.#main.innerHTML = this.#fallbackHTML;
+            return { absPath: '404', segmentHTML: this.#fallbackHTML };
+        }
+        
         this.#main.innerHTML = segmentHTML;
         return { absPath, segmentHTML };
     }
 
     renderPathElement(absPath, element) {
-        element.innerHTML = this.router.getSegmentHTML(absPath);
+        element.innerHTML = this.#router.getSegmentHTML(absPath);
     }
 }
